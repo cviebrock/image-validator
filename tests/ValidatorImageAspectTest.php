dc06855a9e0b7fddfb30ba322942f4fb05c9be98
Colin Viebrock <colin@viebrock.ca>
2013-12-20 11:41:18 -0600

    update tests, adding aspect checks

---
+++ tests/ValidatorImageAspectTest.php
@@ -0,0 +1,81 @@
+<?php
+
+use Cviebrock\ImageSizeValidator\ImageSizeValidator;
+
+class ValidatorImageAspectTest extends PHPUnit_Framework_TestCase
+{
+	protected $translator;
+	protected $data;
+	protected $rules;
+	protected $messages;
+
+
+	public function setUp()
+	{
+		$this->translator = Mockery::mock('Symfony\Component\Translation\TranslatorInterface');
+		$this->translator->shouldReceive('trans');
+		$this->data = array(
+			'image' => dirname(__FILE__) . '/images/200x250.png'
+		);
+	}
+
+
+	public function tearDown()
+	{
+		Mockery::close();
+	}
+
+
+	public function testValidatesAspect()
+	{
+
+		$validator = new ImageSizeValidator(
+			$this->translator,
+			$this->data,
+			array( 'image' => 'image_aspect:4,5' ),
+			$this->messages
+		);
+
+		$this->assertTrue($validator->passes());
+	}
+
+	public function testValidatesAspectDecimal()
+	{
+
+		$validator = new ImageSizeValidator(
+			$this->translator,
+			$this->data,
+			array( 'image' => 'image_aspect:0.8' ),
+			$this->messages
+		);
+
+		$this->assertTrue($validator->passes());
+	}
+
+	public function testValidatesReverseAspect()
+	{
+
+		$validator = new ImageSizeValidator(
+			$this->translator,
+			$this->data,
+			array( 'image' => 'image_aspect:~5,4' ),
+			$this->messages
+		);
+
+		$this->assertTrue($validator->passes());
+	}
+
+	public function testValidatesReverseAspectDecimal()
+	{
+
+		$validator = new ImageSizeValidator(
+			$this->translator,
+			$this->data,
+			array( 'image' => 'image_aspect:~1.25' ),
+			$this->messages
+		);
+
+		$this->assertTrue($validator->passes());
+	}
+
+}
