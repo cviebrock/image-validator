4479e17ee0cae4402a651672b4f33c02611571ea
Arda Kılıçdağı <soulsmasher@gmail.com>
2014-04-02 18:27:25 +0300

    Added Turkish language file for validation.php

---
+++ src/lang/tr/validation.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author Arda Kılıçdağı
+ * @web http://arda.pw
+ */
+
+return array(
+    'image_size'          => ':attribute :width genişliğinde :height yüksekliğinde olmalıdır.',
+
+    'between'            => ':size1 ve :size2 piksel aralığında',
+    'lessthan'           => ':size pikselden daha az',
+    'lessthanorequal'    => ':size piksele eşit veya daha az',
+    'greaterthan'        => ':size pikselden daha fazla',
+    'greaterthanorequal' => ':size piksele eşit veya daha fazla',
+    'equal'              => ':size piksel',
+    'anysize'            => 'herhangi bir boyda',
+
+    'image_aspect'       => ':attribute resim ölçüsü :aspect olmalıdır.',
+);
