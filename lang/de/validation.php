fd7303a445190ef089a0fc32641e88ce185f5d81
Gregory Plüss <gpluess@sunrise.ch>
2015-04-30 22:12:23 +0200

    Added German translation

---
+++ lang/de/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'image_size'          => ':attribute muss :width breit und :height hoch sein.',
+
+	'between'            => 'zwischen :size1 und :size2 Pixel',
+	'lessthan'           => 'kleiner als :size Pixel',
+	'lessthanorequal'    => 'höchstens :size Pixel',
+	'greaterthan'        => 'größer als :size Pixel',
+	'greaterthanorequal' => 'mindestens :size Pixel',
+	'equal'              => ':size Pixel',
+	'anysize'            => 'irgendeine Größe',
+
+	'image_aspect'       => 'Das Seitenverhältnis von :attribute muss :aspect sein',
+);
