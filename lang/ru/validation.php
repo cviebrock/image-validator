82d7b26acebe08a6f2b6c9a094010a1566729bac
Данил <danil@rutorika.ru>
2015-10-07 17:36:51 +0300

    Russian localization

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -2,12 +2,12 @@
 
 return array(
 	'image_size'          => 'Изображение :attribute должно быть :width по ширине и :height по высоте.',
-	'between'            => 'между :size1 и :size2 пкс',
-	'lessthan'           => 'меньше, чем :size пкс',
-	'lessthanorequal'    => 'меньше, либо равно :size пкс',
-	'greaterthan'        => 'больше, чем :size пкс',
-	'greaterthanorequal' => 'больше или равно :size пкс',
-	'equal'              => ':size пкс',
+	'between'            => 'между :size1 и :size2  пикселей(я)',
+	'lessthan'           => 'меньше, чем :size  пикселей(я)',
+	'lessthanorequal'    => 'меньше или равно :size  пикселей(я)',
+	'greaterthan'        => 'больше, чем :size  пикселей(я)',
+	'greaterthanorequal' => 'больше или равно :size пикселей(я)',
+	'equal'              => ':size пикселей(я)',
 	'anysize'            => 'любого размера',
 
 	'image_aspect'       => ':attribute сжатие должно быть :aspect.',
