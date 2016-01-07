7625b2c600b28cc619e5758230d7e36de188ed92
neomusic <g.aremare@palmabit.com>
2016-01-07 10:37:21 +0100

    Add localized files for italian

---
+++ lang/it/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+  'image_size'          => 'Il campo :attribute dev\'essere :width in larghezza and :height in altezza.',
+
+  'between'            => 'tra i :size1 e :size2 pixels',
+  'lessthan'           => 'minore di :size pixels',
+  'lessthanorequal'    => 'minore o uguale a :size pixels',
+  'greaterthan'        => 'maggiore di :size pixels',
+  'greaterthanorequal' => 'maggiore o uguale a :size pixels',
+  'equal'              => 'di :size pixels',
+  'anysize'            => 'qualunque dimensione',
+
+  'image_aspect'       => 'Il campo :attribute deve avere un ratio uguale a :aspect.',
+);
