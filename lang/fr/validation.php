cd2ee9849b2d2d5e9affd0f7826d167da61195f4
Juju <djjuju87000@gmail.com>
2016-04-10 17:58:38 +0200

    Update french translation

--- lang/fr/validation.php
+++ lang/fr/validation.php
@@ -1,15 +1,15 @@
 <?php
 
 return array(
-  'image_size'         => ':attribute doit être de :width de large et :height de haut',
+  'image_size'         => 'La taille de :attribute doit être :width de large et :height de haut',
 
   'between'            => 'entre :size1 et :size2 pixels',
-  'lessthan'           => 'moins de :size pixels',
-  'lessthanorequal'    => 'plus petit ou égal à :size pixels',
-  'greaterthan'        => 'plus grand que :size pixels',
-  'greaterthanorequal' => 'plus grand ou égal à :size pixels',
-  'equal'              => ':size pixels',
-  'anysize'            => "n'importe quelle taille",
+  'lessthan'           => 'plus petite que :size pixels',
+  'lessthanorequal'    => 'plus petite ou égale à :size pixels',
+  'greaterthan'        => 'plus grande que :size pixels',
+  'greaterthanorequal' => 'plus grande ou égale à :size pixels',
+  'equal'              => 'de :size pixels',
+  'anysize'            => "de n'importe quelle taille",
 
   'image_aspect'       => 'Le ratio de :attribute doit être de :aspect',
 );
