ffe37538255e8248314ea10d1dcd9f9795dd6589
Edgars Loskots <el13001@lu.lv>
2016-05-25 14:11:16 +0300

    Added Latvian language

---
+++ lang/lv/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'image_size'          => ':attribute jâbût :width platam un :height augstam.',
+
+	'between'            => 'starp :size1 un :size2 pikseïiem(i)',
+	'lessthan'           => 'mazâkam par :size pikseïiem(i)',
+	'lessthanorequal'    => 'mazâkam vai vienâdam par :size pikseïiem(i)',
+	'greaterthan'        => 'lielâkam par :size pikseïiem(i)',
+	'greaterthanorequal' => 'lielâkam vai vienâdam par :size pikseïiem(i)',
+	'equal'              => ':size pikseïus(i)',
+	'anysize'            => 'jebkura izmçra',
+
+	'image_aspect'       => ':attribute malu attiecîbai jâbût :aspect.',
+);
