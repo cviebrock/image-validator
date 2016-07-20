360cec6c49a6d37cae91bf1125c9ad346c8a5885
Cara Wang <cara4bear@gmail.com>
2016-07-20 15:27:32 +0800

    Add validation in zh-tw language

---
+++ lang/zh-tw/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'image_size'          => ':attribute 欄位的寬度 :width, 而高度 :height',
+
+    'between'            => '必須介於 :size1 與 :size2 像素之間',
+    'lessthan'           => '必須小於 :size 像素',
+    'lessthanorequal'    => '必須小於或等於 :size 像素',
+    'greaterthan'        => '必須大於 :size 像素',
+    'greaterthanorequal' => '必須大於或等於 :size 像素',
+    'equal'              => '必須等於 :size 像素',
+    'anysize'            => '可以是任意大小',
+
+    'image_aspect'       => ':attribute 欄位的長寬比必須是 :aspect',
+);
