3b569369d1cb851e2a15a669c5593a12142fa275
Steel Wood <zougangmu@qq.com>
2016-08-12 15:55:44 +0800

    add Simple Chinese Language File
    add Simple Chinese Language translation File

---
+++ lang/zh-CN/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'image_size'         => ':attribute 的宽度为 :width, 高度为 :height',
+    
+    'between'            => '必须在 :size1 和 :size2 像素之间',
+    'lessthan'           => '必须小于 :size 像素',
+    'lessthanorequal'    => '必须小于或等于 :size 像素',
+    'greaterthan'        => '必须大于 :size 像素',
+    'greaterthanorequal' => '必须大于或等于 :size 像素',
+    'equal'              => '必须等于 :size 像素',
+    'anysize'            => '可以为任意大小',
+    
+    'image_aspect'       => ':attribute 长宽比必须为 :aspect',
+);
