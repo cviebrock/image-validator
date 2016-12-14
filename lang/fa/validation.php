5800f9717e4c7495fe73abd556b5e23cdf675212
saeed sajadi <smart_twists@yahoo.co.uk>
2016-12-14 11:31:46 +0330

    typo fixed

--- lang/fa/validation.php
+++ lang/fa/validation.php
@@ -1,15 +1,15 @@
 <?php
 
 return array(
-	'image_size'          => ':attribute باید دارای صول :width و عرص  :height باشد.',
+	'image_size'          => ':attribute باید دارای طول :width و عرض  :height باشد.',
 
 	'between'            => 'بین  :size1 و :size2 پیکسل باید باشد',
-	'lessthan'           => 'کمتر از  :size پیکسل',
-	'lessthanorequal'    => 'گوچکتر یا برابر :size پیکسل',
-	'greaterthan'        => 'بزرکتر از :size پیکسل',
-	'greaterthanorequal' => 'بزرکتر یا برابر :size پیکسل',
+	'lessthan'           => 'کوچکتر از  :size پیکسل',
+	'lessthanorequal'    => 'کوچکتر یا برابر :size پیکسل',
+	'greaterthan'        => 'بزرگتر از :size پیکسل',
+	'greaterthanorequal' => 'بزرگتر یا برابر :size پیکسل',
 	'equal'              => ':size پیکسل',
 	'anysize'            => 'هر اندازه ای',
 
-	'image_aspect'       => ':attribute تناسب طول و عرض برابر :aspect باید باشد.',
+	'image_aspect'       => ':attribute تناسب طول عرص برابر :aspect باید باشد.',
 );
