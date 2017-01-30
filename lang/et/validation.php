a5f57ceea8db027c18c5338659b772dc9d1143ef
Kaido <kaido@smartsoft.ee>
2017-01-30 16:34:13 +0200

    Added: Estonian language

---
+++ lang/et/validation.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'image_size' => 'Pilt peab olema :width laiust ja :height kõrgust.',
+    'between' => 'between :size1 and :size2 pixels',
+    'lessthan' => 'vähem, kui :size pikslit',
+    'lessthanorequal' => 'väiksem või võrdne :size pikslit',
+    'greaterthan' => 'suurem, kui :size pikslit',
+    'greaterthanorequal' => 'suurem või võrdne :size pikslit',
+    'equal' => ':size pikslit',
+    'anysize' => 'iga suurus',
+    'image_aspect' => ':attribute kuvasuhe peab olema :aspect.',
+];
+
